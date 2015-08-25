059649cbf3a7a26a022afa8a391ddfdfb8b2c912
Justin Taylor <justintaylor@Alderan.local>
2015-08-24 21:58:17 -0400

    Setup bash_env and fixed logic on when to prompt for overwrite

--- install.sh
+++ install.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
+
+if [ ! -f "$HOME/.bash_env" ]; then
+  echo "Create a new .bash_env file using the template file"
+  exit -1
+fi
+
 FILES=./files/*
 for f in $FILES
 do
   filename=$(basename $f)
-  destination="~/.$f"
+  destination="$HOME/.$filename"
 
   con="Y"
-  if [ -f $f ]; then
+  echo $destination
+  if [ -f $destination ]; then
     echo "Overwrite $filename? (Y/n)"
     read con
   fi
